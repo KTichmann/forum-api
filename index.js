3a1970f27699e2a6609c76b6c52655119f2f3b94
unknown <ktichmann@gmail.com>
2019-02-22 15:07:30 +0200

    test insert

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const { Client } = require('pg');
 
 const PORT = process.env.PORT || 5000
 const { DATABASE_URL } = process.env;
-
 const server = http.createServer((req, res) => {
-    const client = new Client({
-        connectionString: DATABASE_URL,
+  const client = new Client({
+    connectionString: DATABASE_URL,
+  });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  client.connect()
+    .then(() => {
+        const query = {
+            text: 'INSERT INTO hellotable (name) VALUES ($1)',
+            values: ['Inserting into the db works!']
+        }
+
+        return client.query(query)
     })
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    client.connect()
-        .then(() => client.query('SELECT * FROM hellotable'))
-        .then(result => {
-            res.end(`${result.rows[0].name}\n`);
-            client.end();
-        })
-        .catch(() => {
-            res.end('ERROR');
-            client.end();
-        })
-})
+    .then(() => client.query('SELECT * FROM hellotable'))
+    .then((result) => {
+      res.end(`${result.rows[1].name}\n`);
+      client.end();
+    })
+    .catch((err) => {
+      res.end(err.toString());
+      client.end();
+    });
+});
 server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  // eslint-disable-next-line
+  console.log(`Server running on ${PORT}/`);
 });
